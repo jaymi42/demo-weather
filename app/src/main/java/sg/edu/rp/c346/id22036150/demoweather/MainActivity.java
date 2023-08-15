d6bc488c82cd89887a021aacc4e28f256a5ced69
jaymi <22036150@myrp.edu.sg>
2023-08-15 09:36:38 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22036150/demoweather/MainActivity.java
@@ -0,0 +1,69 @@
+package sg.edu.rp.c346.id22036150.demoweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvWeather;
+    AsyncHttpClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvWeather = findViewById(R.id.lvWeather);
+        client = new AsyncHttpClient();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ArrayList<Weather> alWeather = new ArrayList<Weather>();
+        client.get("https://api.data.gov.sg/v1/environment/2-hour-weather-forecast", new JsonHttpResponseHandler() {
+            String area;
+            String forecast;
+
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    JSONArray jsonArrItems = response.getJSONArray("items");
+                    JSONObject firstObj = jsonArrItems.getJSONObject(0);
+                    JSONArray jsonArrForecasts = firstObj.getJSONArray("forecasts");
+                    for (int i = 0; i < jsonArrForecasts.length(); i++) {
+                        JSONObject jsonObjForecast = jsonArrForecasts.getJSONObject(i);
+                        area = jsonObjForecast.getString("area");
+                        forecast = jsonObjForecast.getString("forecast");
+                        Weather weather = new Weather(area, forecast);
+                        alWeather.add(weather);
+                    }
+                    ArrayAdapter<Weather> adapter = new ArrayAdapter<Weather>(MainActivity.this, android.R.layout.simple_list_item_1, alWeather);
+                    lvWeather.setAdapter(adapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //POINT X – Code to display List View
+                ArrayAdapter<Weather> adapter = new ArrayAdapter<Weather>(MainActivity.this, android.R.layout.simple_list_item_1, alWeather);
+                lvWeather.setAdapter(adapter);
+
+            }//end onSuccess
+
+        });
+    }//end onResume
+
+}
